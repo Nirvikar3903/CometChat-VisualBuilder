91f209b050340b1052b6802735f14b553d446b82
Nirvikar3903 <nirvikar03.k@gmail.com>
2025-04-26 10:45:50 +0530

    Feedback for CometChat Visual Builder

---
+++ src/main.tsx
@@ -0,0 +1,51 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  UIKitSettingsBuilder,
+  CometChatUIKit,
+} from "@cometchat/chat-uikit-react";
+import { setupLocalization } from "./CometChat/utils/utils";
+import { BuilderSettingsProvider } from "./CometChat/context/BuilderSettingsContext";
+
+export const COMETCHAT_CONSTANTS = {
+  APP_ID: "2737806715563d9b", // Replace with your App ID
+  REGION: "in", // Replace with your App Region
+  AUTH_KEY: "1ffa2a4fdc613bcb1e3c76e68af32c36e2173b01", // Replace with your Auth Key or leave blank if you are authenticating using Auth Token
+};
+
+const uiKitSettings = new UIKitSettingsBuilder()
+  .setAppId(COMETCHAT_CONSTANTS.APP_ID)
+  .setRegion(COMETCHAT_CONSTANTS.REGION)
+  .setAuthKey(COMETCHAT_CONSTANTS.AUTH_KEY)
+  .subscribePresenceForAllUsers()
+  .build();
+
+CometChatUIKit.init(uiKitSettings)?.then(() => {
+  setupLocalization();
+
+  const UID = "cometchat-uid-4"; // Replace with your actual UID
+
+  CometChatUIKit.getLoggedinUser().then((user: CometChat.User | null) => {
+    if (!user) {
+      CometChatUIKit.login(UID)
+        .then((loggedUser: CometChat.User) => {
+          console.log("Login Successful:", loggedUser);
+          // Mount your app
+          ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+            <BuilderSettingsProvider>
+              <App />
+            </BuilderSettingsProvider>
+          );
+        })
+        .catch((error) => console.error("Login Failed:", error));
+    } else {
+      // User already logged in, mount app directly
+      console.log("User already logged in:", user);
+      ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+        <BuilderSettingsProvider>
+          <App />
+        </BuilderSettingsProvider>
+      );
+    }
+  });
+});
